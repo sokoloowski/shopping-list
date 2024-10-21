2b6574a0b58519a15e35b1c4e9dbdc0299e52c53
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2024-10-21 10:40:30 +0200

    Shopping list tests

---
+++ tests/Entity/ShoppingListTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Product;
+use App\Entity\ShoppingList;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class ShoppingListTest extends TestCase
+{
+    public function testWhenShoppingListIsCreated_ThenOwnerIsAssigned(): void
+    {
+        $user = $this->createMock(User::class);
+        $list = new ShoppingList();
+        $list->setOwner($user);
+        $this->assertSame($user, $list->getOwner());
+    }
+
+    public function testWhenProductIsAddedToShoppingList_ThenProductIsAddedToItems(): void
+    {
+        $list = new ShoppingList();
+        $product = $this->createMock(Product::class);
+        $list->addProduct($product);
+        $this->assertContains($product, $list->getItems());
+    }
+
+    public function testWhenProductIsAddedToShoppingList_ThenProductShoppingListWillBeSet(): void
+    {
+        $list = new ShoppingList();
+        $product = $this->createMock(Product::class);
+        $product->expects($this->once())
+            ->method("setShoppingList")
+            ->with($list);
+        $list->addProduct($product);
+    }
+
+    public function testWhenShoppingListIsCreated_ThenNameIsNotNull(): void
+    {
+        $list = new ShoppingList();
+        $this->assertNotNull($list->getName());
+    }
+
+    public function testWhenShoppingListIsCreated_ThenNameIsNotEmpty(): void
+    {
+        $list = new ShoppingList();
+        $this->assertNotEmpty($list->getName());
+    }
+
+    public function testWhenNameIsSet_ThenCorrectNameIsGet(): void
+    {
+        $list = new ShoppingList();
+        $name = "[TEST] Some shopping list name";
+        $list->setName($name);
+        $this->assertEquals($name, $list->getName());
+    }
+
+    public function testWhenUserIsCreated_ThenIdIsAssigned(): void
+    {
+        $list = new ShoppingList();
+        $this->assertNotNull($list->getId());
+    }
+
+    public function testWhenPurchaseDateIsSet_ThenCorrectDateIsGet(): void
+    {
+        $list = new ShoppingList();
+        $date = new \DateTimeImmutable();
+        $list->setPurchaseDate($date);
+        $this->assertEquals($date, $list->getPurchaseDate());
+    }
+}
