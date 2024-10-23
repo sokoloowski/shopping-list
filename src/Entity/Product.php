5926649ebf257bf5f0c50659090467bfce239e00
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2024-10-23 09:15:52 +0200

    Add Product entity with related method in ShoppingList

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -13,8 +13,91 @@ class Product
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $quantity = null;
+
+    #[ORM\Column]
+    private bool $realisation = false;
+
+    #[ORM\Column(enumType: ProductUnitEnum::class)]
+    private ?ProductUnitEnum $unit = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ShoppingList $shoppingList = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?float
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(float $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function isRealised(): ?bool
+    {
+        return $this->realisation;
+    }
+
+    public function setRealisation(bool $realisation): static
+    {
+        $this->realisation = $realisation;
+
+        return $this;
+    }
+
+    public function toggleRealisation(): static
+    {
+        $this->realisation = !$this->realisation;
+
+        return $this;
+    }
+
+    public function getUnit(): ?ProductUnitEnum
+    {
+        return $this->unit;
+    }
+
+    public function setUnit(ProductUnitEnum $unit): static
+    {
+        $this->unit = $unit;
+
+        return $this;
+    }
+
+    public function getShoppingList(): ?ShoppingList
+    {
+        return $this->shoppingList;
+    }
+
+    public function setShoppingList(?ShoppingList $shoppingList): static
+    {
+        $this->shoppingList = $shoppingList;
+
+        return $this;
     }
 }
--- src/Entity/ShoppingList.php
+++ src/Entity/ShoppingList.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ShoppingListRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ShoppingListRepository::class)]
@@ -13,8 +15,50 @@ class ShoppingList
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * @var Collection<int, Product>
+     */
+    #[ORM\OneToMany(targetEntity: Product::class, mappedBy: 'shoppingList', orphanRemoval: true)]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+        $this->name = "New shopping list on " . (new \DateTimeImmutable())->format("Y-m-d H:i:s");
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): static
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->setShoppingList($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): static
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getShoppingList() === $this) {
+                $product->setShoppingList(null);
+            }
+        }
+
+        return $this;
     }
 }
