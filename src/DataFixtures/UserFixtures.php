0f5dfacc216cf9052acc7beb1e00bda3353cff17
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2024-10-28 10:21:45 +0100

    Create User fixtures for testing

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+    )
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $userJanKowalski = new User();
+        $hashedPassword = $this->passwordHasher->hashPassword($userJanKowalski, "password123");
+        $userJanKowalski->setEmail("jan.kowalski@example.com")
+            ->setUsername("jkowalski")
+            ->setPassword($hashedPassword);
+        $manager->persist($userJanKowalski);
+
+        $userJohnDoe = new User();
+        $hashedPassword = $this->passwordHasher->hashPassword($userJohnDoe, "password123");
+        $userJohnDoe->setEmail("john.doe@example.com")
+            ->setUsername("johndoe")
+            ->setPassword($hashedPassword);
+        $manager->persist($userJohnDoe);
+
+        $manager->flush();
+    }
+}
