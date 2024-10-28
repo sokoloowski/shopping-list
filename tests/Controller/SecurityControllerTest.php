d195c513d95200d4ef950652a26ed9c1c1b427c9
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2024-10-28 10:27:06 +0100

    Unit and system tests for logging in and out

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\DataFixtures\UserFixtures;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testWhenClientWantsToLogIn_ThenLoginPageExists(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request("GET", "/login");
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testWhenOnLoginPage_ThenEmailInputIsShown(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request("GET", "/login");
+        self::assertSelectorExists("input[type=email]");
+    }
+
+    public function testWhenOnLoginPage_ThenPasswordInputIsShown(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request("GET", "/login");
+        self::assertSelectorExists("input[type=password]");
+    }
+
+    public function testWhenOnLoginPage_ThenSubmitButtonIsShown(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request("GET", "/login");
+        self::assertSelectorExists("[type=submit]");
+    }
+
+    public function testWhenOnLoginPage_ThenRegisterButtonIsShown(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request("GET", "/login");
+        self::assertSelectorExists("a");
+    }
+
+    public function testWhenUserTriesToUseWrongPassword_ThenMessageIsShown(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request("GET", "/login");
+        $client->submitForm("Sign in", [
+            "_username" => "jan.kowalski@example.com",
+            "_password" => "thisPasswordIsIncorrect"
+        ]);
+        $client->followRedirect();
+        self::assertSelectorExists(".alert-danger");
+        self::assertSelectorTextContains(".alert-danger", "Invalid credentials");
+    }
+
+    /**
+     * @see UserFixtures
+     */
+    public function testWhenUserTriesToUseCorrectPassword_ThenUserIsRedirectedToHome(): void
+    {
+        $username = "jan.kowalski@example.com";
+        $client = self::createClient();
+        $crawler = $client->request("GET", "/login");
+        $client->submitForm("Sign in", [
+            "_username" => $username,
+            "_password" => "password123"
+        ]);
+        self::assertResponseRedirects("/");
+
+        # check if user is logged in
+        $crawler = $client->request("GET", "/login");
+        self::assertSelectorExists(".alert-warning");
+        self::assertSelectorTextContains(".alert-warning", "You are logged in");
+        self::assertSelectorTextContains(".alert-warning", $username);
+    }
+
+    public function testWhenUserLogsOut_ThenUserIsRedirectedToHome(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request("GET", "/logout");
+        self::assertResponseRedirects("/");
+
+        # check if user is logged out
+        $crawler = $client->request("GET", "/login");
+        self::assertSelectorNotExists(".alert-warning");
+    }
+}
